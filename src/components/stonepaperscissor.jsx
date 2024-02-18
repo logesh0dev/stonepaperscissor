22c3d421e4e3c6944f79452ef85b2fb2781932de
logesh <logeshshankar2001@gmail.com>
2024-02-18 18:41:59 +0530

    imges changes

--- src/components/stonepaperscissor.jsx
+++ src/components/stonepaperscissor.jsx
@@ -9,9 +9,9 @@ import { Celebration } from './celebration';
 export const Stonepaperscissor = () => {
 
     const options = [
-        { name: 'Stone', id: 1, src: '/src/assets/stone.png' },
-        { name: 'Paper', id: 2, src: '/src/assets/paper.png' },
-        { name: 'Scissor', id: 3, src: '/src/assets/scissors.png' }
+        { name: 'Stone', id: 1, src: '../src/assets/stone.png' },
+        { name: 'Paper', id: 2, src: '../src/assets/paper.png' },
+        { name: 'Scissor', id: 3, src: '../src/assets/scissors.png' }
     ]
 
     var [opponent, setopponentvalue] = useState(0);
@@ -49,19 +49,19 @@ export const Stonepaperscissor = () => {
             setwinner('Wins')
             setplayerwinningcount(playerwinningcount + 1)
             // playerwinningcount++
-            setscorecard(prev => [...prev, { name: 'Player', src: "/src/assets/player.png" }]);
+            setscorecard(prev => [...prev, { name: 'Player', src: "./src/assets/player.png" }]);
         }
         else if (value.id == b.id) {
             setwinner('Draw');
             // setscorecard(prev => [...prev, 'Draw'])
-            setscorecard(prev => [...prev, { name: 'Draw', src: "/src/assets/equal.png" }]);
+            setscorecard(prev => [...prev, { name: 'Draw', src: "./src/assets/equal.png" }]);
 
         }
         else {
             setwinner('Lose')
             setpcomputerwinningcount(computerwinningcount + 1)
             // setscorecard(prev => [...prev, 'Computer'])
-            setscorecard(prev => [...prev, { name: 'Computer', src: "/src/assets/ai.png" }]);
+            setscorecard(prev => [...prev, { name: 'Computer', src: "./src/assets/ai.png" }]);
 
             // computerwinningcount++
 
@@ -131,7 +131,7 @@ export const Stonepaperscissor = () => {
 
                 <div className='clashicon' >
 
-                    <img src={'/src/assets/sword.png'} className="logo react" alt="Vite logo" />
+                    <img src={'./src/assets/sword.png'} className="logo react" alt="Vite logo" />
                     <div>
 
                         {/* <IconButton  size="large" color="secondary">
